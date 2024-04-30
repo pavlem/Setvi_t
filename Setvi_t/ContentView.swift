910848f6458821217d6de994fe4355e126dcb5d1
Pavle Mijatovic <paul.mijatovic@gmail.com>
2024-04-30 14:31:38 +0200

    Initial commit

---
+++ Setvi_t/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Setvi_t
+//
+//  Created by Pavle on 30.4.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Setvi_t/Setvi_tApp.swift
@@ -0,0 +1,17 @@
+//
+//  Setvi_tApp.swift
+//  Setvi_t
+//
+//  Created by Pavle on 30.4.24..
+//
+
+import SwiftUI
+
+@main
+struct Setvi_tApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
